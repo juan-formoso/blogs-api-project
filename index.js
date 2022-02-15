9b1cab2159f2cda45ca19dd31b68945966a82e0e
Juan Formoso <trajano.juan@gmail.com>
2022-02-15 12:35:08 -0300

    Add new files

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const userRouter = require('./routes/user');
 
 const app = express();
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
+
+app.use('/user', userRouter);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
