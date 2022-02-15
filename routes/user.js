0af52eccb9ad8fd53096d642f50bedf61c3db379
Juan Formoso <trajano.juan@gmail.com>
2022-02-15 18:59:03 -0300

    Add usersRoute

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+const userController = require('../controllers/user');
+const authMiddleware = require('../helpers/authentification');
 
-router.post('/');
+router.post('/', userController.createUser);
+router.get('/', authMiddleware, userController.getUsers);
+router.get('/:id', authMiddleware, userController.getUserById);
+router.delete('/', authMiddleware, userController.deleteUser);
 
 module.exports = router;
