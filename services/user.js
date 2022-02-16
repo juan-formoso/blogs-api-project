88dbbb70a8dca8af838e57c5e644c9381d05a4fa
Juan Formoso <trajano.juan@gmail.com>
2022-02-15 23:19:26 -0300

    Update userServices

--- services/user.js
+++ services/user.js
@@ -3,71 +3,45 @@ const JoiSchema = require('../helpers/schemas');
 const userModel = require('../models/user');
 require('dotenv').config();
 
-const userValidation = async (req, res, next) => {
-  const { error } = JoiSchema.userSchema.validate(req.body);
-  if (error) {
-    return res.status(400).json({
-      message: error.details[0].message,
-    });
-  }
-  const ifEmailExists = await userModel.findOne({ email: req.body.email });
-  if (ifEmailExists) {
-    return res.status(409).json({
-      message: 'User already registered',
-    });
-  }
-  next();
+const userValidation = async ({ displayName, email, password, image }) => {
+  const { error } = JoiSchema.userSchema.validate({ displayName, email, password, image });
+  if (error) return { code: 400, message: error.details[0].message };
+  const ifEmailExists = await userModel.findOne({ where: { email } });
+  if (ifEmailExists) return { code: 409, message: 'User already registered' };
+  return {};
 };
 
-const createUser = async (req, res) => {
-  const { code, message } = await userValidation(req, res);
-  if (code) {
-    return res.status(code).json({
-      message,
-    });
-  }
-  const user = await userModel.create(req.body);
-  const { password: _, ...userData } = user.dataValues;
-  const token = jwt.sign(userData, process.env.JWT_SECRET, {
+const createUser = async ({ displayName, email, password, image }) => {
+  const { code, message } = await userValidation({ displayName, email, password, image });
+  if (code) return { code, message };
+  const user = await userModel.create({ displayName, email, password, image });
+  const { password: _, ...userCreated } = user.dataValues;
+  const token = jwt.sign(userCreated, process.env.JWT_SECRET, {
     algorithm: 'HS256',
     expiresIn: '60h',
   });
   return { token };
 };
 
-const getAllUsers = async (_req, _res) => {
+const getAllUsers = async () => {
   const allUsers = await userModel.findAll();
   return allUsers;
 };
 
-const getUserById = async (req, res) => {
-  const { id } = req.params;
-  const user = await userModel.findOne({ where: { id } });
-  if (!user) {
-    return res.status(404).json({
-      message: 'User does not exist',
-    });
-  }
-  return user;
+const getUserById = async (id) => {
+  const user = await userModel.findByPk(id);
+  return user || { code: 404, message: 'User does not exist' };
 };
 
-const deleteUser = async (req, res) => {
-  const { id } = req.params;
-  const user = await userModel.findOne({ where: { id } });
-  if (!user) {
-    return res.status(404).json({
-      message: 'User does not exist',
-    });
-  }
-  await user.destroy();
-  return res.status(200).json({
-    message: 'User deleted successfully',
-  });
+const deleteUser = async (token) => {
+  const { id } = jwt.decode(token, process.env.JWT_SECRET);
+  const deletedUser = await userModel.destroy({ where: { id } });
+  return deletedUser;
 };
 
 module.exports = {
+  deleteUser,
   createUser,
   getAllUsers,
   getUserById,
-  deleteUser,
 };
