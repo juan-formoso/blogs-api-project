d8a2fc3969a98c0d5483adab325c81cc2529e245
Juan Formoso <trajano.juan@gmail.com>
2022-02-15 23:52:16 -0300

    Update loginController

--- controllers/login.js
+++ controllers/login.js
@@ -1,13 +1,11 @@
 const rescue = require('express-rescue');
-const loginService = require('../services/login');
+const loginServices = require('../services/login');
 
 const login = rescue(async (req, res) => {
   const { email, password } = req.body;
-  const user = await loginService.login(email, password);
-  if (user.code) {
-    return res.status(user.code).json({ message: user.message });
-  }
-  return res.status(200).json(user);
+  const loginUser = await loginServices.login({ email, password });
+  if (loginUser.code) return res.status(loginUser.code).json({ message: loginUser.message });
+  return res.status(200).json(loginUser);
 });
 
 module.exports = { login };
