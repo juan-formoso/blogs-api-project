2f4c3c2cd56af9ab43c1c7b78a511d0cc9dbac23
Juan Formoso <trajano.juan@gmail.com>
2022-02-15 22:53:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const userRouter = require('./routes/user');
-const categoryRouter = require('./routes/category');
-const loginRouter = require('./routes/login');
+const userRouter = require('./routers/user.routes');
+const loginRouter = require('./routers/login.routes');
+const categoriesRouter = require('./routers/postsCategories.routes');
+const blogPostsRouter = require('./routers/blogPosts.routes');
 const errorMiddleware = require('./helpers/error');
 const authMiddleware = require('./helpers/auth');
 
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
 app.use('/user', userRouter);
-app.use('/categories', categoryRouter);
 app.use('/login', loginRouter);
 app.use(authMiddleware);
+app.use('/categories', categoriesRouter);
+app.use('/post', blogPostsRouter);
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
