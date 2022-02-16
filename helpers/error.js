58d2b080c1d2eb0b681fed16191c6b9b4d13c254
Juan Formoso <trajano.juan@gmail.com>
2022-02-15 22:55:16 -0300

    Update error.js

--- helpers/error.js
+++ helpers/error.js
@@ -1,4 +1,5 @@
-module.exports = (err, req, res, _next) => {
-  console.log(err);
-  res.status(500).json({ message: 'Internal server error' });
+module.exports = (err, res) => {
+  const status = err.status || 500;
+  const message = err.message || 'Internal server error';
+  res.status(status).json({ message });
 };
