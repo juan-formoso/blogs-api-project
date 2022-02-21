3f707b2d5bd9848234b6c95dd8fe37eee0f295fe
Juan Formoso <trajano.juan@gmail.com>
2022-02-21 19:47:17 -0300

    Refatorando codigo (3)

--- controllers/user.js
+++ controllers/user.js
@@ -1,22 +1,26 @@
 const rescue = require('express-rescue');
 const userServices = require('../services/user');
 
-const createUser = rescue(async (req, res) => {
+const create = rescue(async (req, res) => {
   const { displayName, email, password, image } = req.body;
   const newUser = await userServices.createUser({ displayName, email, password, image });
-  if (newUser.code) return res.status(newUser.code).json({ message: newUser.message });
+  if (newUser.code) {
+    return res.status(newUser.code).json({ message: newUser.message });
+  }
   return res.status(201).json(newUser);
 });
 
-const getAllUsers = rescue(async (_req, res) => {
-  const users = await userServices.getAllUsers();
+const getUsers = rescue(async (_req, res) => {
+  const users = await userServices.getUsers();
   return res.status(200).json(users);
 });
 
 const getUserById = rescue(async (req, res) => {
   const { id } = req.params;
   const user = await userServices.getUserById(id);
-  if (user.code) return res.status(user.code).json({ message: user.message });
+  if (user.code) {
+    return res.status(user.code).json({ message: user.message });
+  }
   return res.status(200).json(user);
 });
 
@@ -26,4 +30,4 @@ const deleteUser = rescue(async (req, res) => {
   return res.status(204).end();
 });
 
-module.exports = { createUser, getAllUsers, getUserById, deleteUser };
+module.exports = { create, getUsers, getUserById, deleteUser };
