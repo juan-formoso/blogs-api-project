b5e3146fc15b5756906dfd1f8a29a929f08109f7
Juan Formoso <trajano.juan@gmail.com>
2022-02-21 19:47:10 -0300

    Refatorando codigo (2)

--- controllers/postsCategories.js
+++ controllers/postsCategories.js
@@ -1,15 +1,17 @@
 const rescue = require('express-rescue');
-const postsCategoriesServices = require('../services/postsCategories');
+const categoryServices = require('../services/postsCategories');
 
 const create = rescue(async (req, res) => {
   const { name } = req.body;
-  const category = await postsCategoriesServices.create(name);
-  if (category.code) return res.status(category.code).json({ message: category.message });
+  const category = await categoryServices.create(name);  
+  if (category.code) {
+    return res.status(category.code).json({ message: category.message });
+  }
   return res.status(201).json(category);
 });
 
-const getCategories = rescue(async (_req, res) => {
-  const category = await postsCategoriesServices.getCategories();
+const getCategories = rescue(async (req, res) => {
+  const category = await categoryServices.getCategories();
   return res.status(200).json(category);
 });
 
