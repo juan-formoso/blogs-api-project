b7e2c5f81bd329721fb01ae788dd65292ca3d1da
Juan Formoso <trajano.juan@gmail.com>
2022-02-21 19:47:46 -0300

    Refatorando codigo (6)

--- services/blogPosts.js
+++ services/blogPosts.js
@@ -1,19 +1,16 @@
 const jwt = require('jsonwebtoken');
 const { Op } = require('sequelize');
-const { User } = require('../models');
-const { Categories } = require('../models');
-const { PostsCategories } = require('../models');
-const { BlogPosts } = require('../models');
-const JoiSchema = require('../helpers/schemas');
+const { BlogPost, Category, User, PostsCategory } = require('../models');
+const { blogPostSchema, updatePostSchema } = require('../helpers/schemas');
 require('dotenv').config();
 
-const decodeToken = (token) => {
+const decodeIdToken = (token) => {
   const { id } = jwt.decode(token, process.env.JWT_SECRET);
   return id;
 };
 
-const categoryValidation = async (categoryIds) => {
-  const categories = categoryIds.map((value) => Categories.findByPk(value));
+const validateCategory = async (categoryIds) => {
+  const categories = categoryIds.map((value) => Category.findByPk(value));
   const exists = await Promise.all(categories);
   if (exists.some((value) => value === null)) {
     return false;
@@ -21,100 +18,117 @@ const categoryValidation = async (categoryIds) => {
   return true;
 };
 
-const blogPostValidation = async ({ title, content, categoryIds }) => {
-  const { error } = JoiSchema.blogPostSchema.validate({ title, content, categoryIds });
-  if (error) return { code: 400, message: error.details[0].message };
-  const categories = await categoryValidation(categoryIds);
-  if (!categories) return { code: 400, message: '"categoryIds" not found' };
+const validateBlogPost = async ({ title, content, categoryIds }) => {
+  const { error } = blogPostSchema.validate({ title, content, categoryIds });
+  if (error) {
+    return { code: 400, message: error.details[0].message };
+  }
+  const categories = await validateCategory(categoryIds);
+  if (!categories) {
+    return { code: 400, message: '"categoryIds" not found' };
+  }
   return {};
 };
 
-const updatePostValidation = (token, { id, title, content, categoryIds }) => {
-  const { error } = JoiSchema.updatePostSchema.validate({ title, content });
-  const tokenId = decodeToken(token);
-  if (error) return { code: 400, message: error.details[0].message };
-  if (categoryIds) return { code: 400, message: 'Categories cannot be edited' };
-  if (tokenId !== +id) return { code: 401, message: 'Unauthorized user' };
+const validateUpdatePost = (token, { id, title, content, categoryIds }) => {
+  const { error } = updatePostSchema.validate({ title, content });
+  const tokenId = decodeIdToken(token);
+  if (error) {
+    return { code: 400, message: error.details[0].message };
+  }
+  if (categoryIds) {
+    return { code: 400, message: 'Categories cannot be edited' };
+  }
+  if (tokenId !== +id) {
+    return { code: 401, message: 'Unauthorized user' };
+  }
   return {};
 };
 
-const createBlogPost = async (userToken, { title, content, categoryIds }) => {
-  const { code, message } = await blogPostValidation({ title, content, categoryIds });
-  const id = decodeToken(userToken);
+const create = async (userToken, { title, content, categoryIds }) => {
+  const { code, message } = await validateBlogPost({ title, content, categoryIds });
+  const id = decodeIdToken(userToken);
   const date = new Date();
-  if (code) return { code, message };
-  const post = await BlogPosts.create(
-    { userId: id, title, content, updated: date, published: date },
-  );
+  if (code) {
+    return { code, message };
+  }
+  const post = await BlogPost
+  .create({ userId: id, title, content, updated: date, published: date });
   const postId = post.dataValues.id;
-  await Promise.all(categoryIds.map((categoryId) => PostsCategories.create(
-    { postId, categoryId },
-  )));
+  await Promise.all(categoryIds.map((categoryId) => PostsCategory.create({ postId, categoryId })));
   const { updated, published, ...blogPost } = post.dataValues;
   return blogPost;
 };
 
 const getPostById = async (id) => {
-  const [categories] = await BlogPosts.findAll({ 
-    where: { id }, 
+  const [categories] = await BlogPost.findAll({
+    where: { id },
     include: [
-      { model: User, as: 'user', attributes: { exclude: ['password'] } },
-      { model: Categories, as: 'categories', through: { attributes: [] } },
-    ], 
+    { model: User, as: 'user', attributes: { exclude: ['password'] } },
+    { model: Category, as: 'categories', through: { attributes: [] } },
+  ]
+    , 
   });
   return categories || { code: 404, message: 'Post does not exist' };
 };
 
-const getAllCategories = async () => {
-  const categories = await BlogPosts.findAll({
+const getAll = async () => {
+  const categories = await BlogPost.findAll({
     include: [
-      { model: User, as: 'user', attributes: { exclude: ['password'] } },
-      { model: Categories, as: 'categories', through: { attributes: [] } },
-    ], 
+    { model: User, as: 'user', attributes: { exclude: ['password'] } },
+    { model: Category, as: 'categories', through: { attributes: [] } },
+  ]
+    , 
   });
   return categories;
 };
 
 const updatePost = async (token, { id, title, content, categoryIds }) => {
-  const { code, message } = updatePostValidation(token, { id, title, content, categoryIds });
-  if (code) return { code, message };
-  await BlogPosts.update({ title, content, updated: new Date() }, { where: { id } });
-  const [post] = await BlogPosts.findAll({ 
+  const { code, message } = validateUpdatePost(token, { id, title, content, categoryIds });
+  if (code) {
+    return { code, message };
+  }
+  await BlogPost.update({ title, content, updated: new Date() }, { where: { id } });
+  const [post] = await BlogPost.findAll({ 
     where: { id },
     attributes: { exclude: ['id', 'published', 'updated'] },
-    include: { model: Categories, as: 'categories', through: { attributes: [] } },
-  });
+    include: { model: Category, as: 'categories', through: { attributes: [] } } });
   return post;
 };
 
 const deletePost = async (token, id) => {
-  const tokenId = decodeToken(token); 
-  const post = await BlogPosts.findByPk(id);
-  if (!post) return { code: 404, message: 'Post does not exist' };
-  if (tokenId !== +id) return { code: 401, message: 'Unauthorized user' };
-  await PostsCategories.destroy({ where: { postId: id } });
-  const deleteBlogPost = await BlogPosts.destroy({ where: { id } });
-  return deleteBlogPost;
+  const tokenId = decodeIdToken(token); 
+  const post = await BlogPost.findByPk(id);
+  if (!post) {
+    return { code: 404, message: 'Post does not exist' };
+  }
+  if (tokenId !== +id) {
+    return { code: 401, message: 'Unauthorized user' };
+  }
+  await PostsCategory.destroy({ where: { postId: id } });
+  const deleteBloPost = await BlogPost.destroy({ where: { id } });
+  return deleteBloPost;
 };
 
 const searchByTitleOrContent = async (searchTerm) => {
-  const findPost = await BlogPosts.findAll({
+  const findPost = await BlogPost.findAll({
     where: { [Op.or]: [
-      { title: { [Op.substring]: searchTerm } },
-      { content: { [Op.substring]: searchTerm } },
+        { title: { [Op.substring]: searchTerm } },
+        { content: { [Op.substring]: searchTerm } },
     ],
-    },  
+    },
     include: [
-      { model: User, as: 'user', attributes: { exclude: ['password'] } },
-      { model: Categories, as: 'categories', through: { attributes: [] } },
-    ], 
+    { model: User, as: 'user', attributes: { exclude: ['password'] } },
+    { model: Category, as: 'categories', through: { attributes: [] } },
+  ]
+    , 
   });
   return findPost || [];
 };
 
 module.exports = {
-  createBlogPost,
-  getAllCategories,
+  create,
+  getAll,
   getPostById,
   updatePost,
   deletePost,
--- services/postsCategories.js
+++ services/postsCategories.js
@@ -1,15 +1,17 @@
-const { Categories } = require('../models');
-const JoiSchema = require('../helpers/schemas');
+const { Category } = require('../models');
+const { categorySchema } = require('../helpers/schemas');
 
 const create = async (name) => {
-  const { error } = JoiSchema.categorySchema.validate({ name });
-  if (error) return { code: 400, message: error.details[0].message };
-  const category = await Categories.create({ name });
+  const { error } = categorySchema.validate({ name });
+  if (error) {
+    return { code: 400, message: error.details[0].message };
+  }
+  const category = await Category.create({ name });
   return category;
 };
 
 const getCategories = async () => {
-  const category = await Categories.findAll();
+  const category = await Category.findAll();
   return category;
 };
 
